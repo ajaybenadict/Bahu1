e226ac323b4b84c3ac00b7acbe6b533ece60efd5
ajaybenadict <ajayantonyraju@gmail.com>
2020-05-27 12:46:25 -0400

    Updates

---
+++ precache-manifest.9790dcc62a1dd0636d6bae65388db6b0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2196acf03724dfddf79cd89edc9e0f36",
+    "url": "/bahu2/index.html"
+  },
+  {
+    "revision": "4b357491876838959cd8",
+    "url": "/bahu2/static/css/main.05966e66.chunk.css"
+  },
+  {
+    "revision": "d6a0ac04925b14d6ed98",
+    "url": "/bahu2/static/js/2.c581f591.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bahu2/static/js/2.c581f591.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b357491876838959cd8",
+    "url": "/bahu2/static/js/main.b6bd35be.chunk.js"
+  },
+  {
+    "revision": "ff92fd3bce15aed66bac",
+    "url": "/bahu2/static/js/runtime-main.a089651d.js"
+  },
+  {
+    "revision": "a09b9b47934769abc7377ea20d9b0925",
+    "url": "/bahu2/static/media/ajay.a09b9b47.jpg"
+  },
+  {
+    "revision": "e3d9a121dd72d11f5ecc5cc64d234008",
+    "url": "/bahu2/static/media/bahu.e3d9a121.jpg"
+  },
+  {
+    "revision": "55ff59fbe70c92c264b29e808befaa53",
+    "url": "/bahu2/static/media/contact.55ff59fb.jpg"
+  },
+  {
+    "revision": "e11ed356319518d0d86446066fa2d300",
+    "url": "/bahu2/static/media/details.e11ed356.jpg"
+  },
+  {
+    "revision": "1bf8e116b0710f8e8776f843cb28c81b",
+    "url": "/bahu2/static/media/joe.1bf8e116.jpg"
+  },
+  {
+    "revision": "e7ac1dff60aaf933d701a90df50d1d6b",
+    "url": "/bahu2/static/media/jude.e7ac1dff.jpg"
+  },
+  {
+    "revision": "a7071ddce5e1e6ab018fa3b437252080",
+    "url": "/bahu2/static/media/login.a7071ddc.jpg"
+  },
+  {
+    "revision": "02acd22c832dcd6924dbd196c09a3e94",
+    "url": "/bahu2/static/media/mass.02acd22c.jpg"
+  },
+  {
+    "revision": "7ac9ef4b949de938640735d3c0fd2dae",
+    "url": "/bahu2/static/media/og1.7ac9ef4b.jpg"
+  },
+  {
+    "revision": "c3c227e1800fc5ee81d138eb7d233a51",
+    "url": "/bahu2/static/media/pandi.c3c227e1.jpg"
+  },
+  {
+    "revision": "e5b8b629d29e858da264a894bf46cc4f",
+    "url": "/bahu2/static/media/raghul.e5b8b629.jpg"
+  },
+  {
+    "revision": "5b51d76cdaf4a50a1895316969dbf55a",
+    "url": "/bahu2/static/media/roy.5b51d76c.jpg"
+  },
+  {
+    "revision": "1819daa1b8c3821a037678b26a283eef",
+    "url": "/bahu2/static/media/sugg2.1819daa1.jpg"
+  },
+  {
+    "revision": "4861c381882fe2d02b3c563b3563661d",
+    "url": "/bahu2/static/media/vedha.4861c381.jpg"
+  }
+]);
